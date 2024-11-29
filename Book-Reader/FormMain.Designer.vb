a4212af0b87d520055d6f542866af5a5b9eb3d21
14thDream <dndelara@proton.me>
2024-11-29 14:29:05 +0800

    Increased minimum size of FormMain.

--- Book-Reader/FormMain.Designer.vb
+++ Book-Reader/FormMain.Designer.vb
@@ -29,28 +29,29 @@ Partial Class FormMain
         ' 
         TableLayoutPanelDashboard.AutoScroll = True
         TableLayoutPanelDashboard.ColumnCount = 4
-        TableLayoutPanelDashboard.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 25.0F))
-        TableLayoutPanelDashboard.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 25.0F))
-        TableLayoutPanelDashboard.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 25.0F))
-        TableLayoutPanelDashboard.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 25.0F))
+        TableLayoutPanelDashboard.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 25F))
+        TableLayoutPanelDashboard.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 25F))
+        TableLayoutPanelDashboard.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 25F))
+        TableLayoutPanelDashboard.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 25F))
         TableLayoutPanelDashboard.Dock = DockStyle.Fill
         TableLayoutPanelDashboard.Location = New Point(0, 0)
         TableLayoutPanelDashboard.Name = "TableLayoutPanelDashboard"
         TableLayoutPanelDashboard.RowCount = 5
-        TableLayoutPanelDashboard.RowStyles.Add(New RowStyle(SizeType.Percent, 20.0F))
-        TableLayoutPanelDashboard.RowStyles.Add(New RowStyle(SizeType.Percent, 20.0F))
-        TableLayoutPanelDashboard.RowStyles.Add(New RowStyle(SizeType.Percent, 20.0F))
-        TableLayoutPanelDashboard.RowStyles.Add(New RowStyle(SizeType.Percent, 20.0F))
-        TableLayoutPanelDashboard.RowStyles.Add(New RowStyle(SizeType.Percent, 20.0F))
-        TableLayoutPanelDashboard.Size = New Size(800, 450)
+        TableLayoutPanelDashboard.RowStyles.Add(New RowStyle(SizeType.Percent, 20F))
+        TableLayoutPanelDashboard.RowStyles.Add(New RowStyle(SizeType.Percent, 20F))
+        TableLayoutPanelDashboard.RowStyles.Add(New RowStyle(SizeType.Percent, 20F))
+        TableLayoutPanelDashboard.RowStyles.Add(New RowStyle(SizeType.Percent, 20F))
+        TableLayoutPanelDashboard.RowStyles.Add(New RowStyle(SizeType.Percent, 20F))
+        TableLayoutPanelDashboard.Size = New Size(1298, 999)
         TableLayoutPanelDashboard.TabIndex = 0
         ' 
         ' FormMain
         ' 
         AutoScaleDimensions = New SizeF(7.0F, 15.0F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(1298, 999)
         Controls.Add(TableLayoutPanelDashboard)
+        MinimumSize = New Size(1314, 755)
         Name = "FormMain"
         Text = "Book Reader"
         ResumeLayout(False)
