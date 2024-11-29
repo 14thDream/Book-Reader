fa6d8727e7e07498627300920812de92a5e1ba87
14thDream <dndelara@proton.me>
2024-11-29 15:10:18 +0800

    Added a border effect when you hover over a Book.

--- Book-Reader/Controls/Book.vb
+++ Book-Reader/Controls/Book.vb
@@ -1,4 +1,5 @@
 ﻿Public Class Book
+
     Public Sub New(Title As String)
 
         ' This call is required by the designer.
@@ -6,6 +7,11 @@
 
         ' Add any initialization after the InitializeComponent() call.
         LabelTitle.Text = Title
+
+        For Each c As Control In PanelBook.Controls
+            AddHandler c.MouseHover, AddressOf AddBorder
+            AddHandler c.MouseLeave, AddressOf RemoveBorder
+        Next
 
     End Sub
 
@@ -15,4 +21,17 @@
         PictureBoxCover.ImageLocation = ImageLocation
 
     End Sub
+
+    Private Sub AddBorder()
+
+        PanelBook.BorderStyle = BorderStyle.FixedSingle
+
+    End Sub
+
+    Private Sub RemoveBorder()
+
+        PanelBook.BorderStyle = BorderStyle.None
+
+    End Sub
+
 End Class
