591b4ac5dc5ac04c792858635f19d158a367aad0
14thDream <dndelara@proton.me>
2024-11-29 13:40:25 +0800

    Added table layout panel.

---
+++ Book-Reader/FormMain.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FormMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TableLayoutPanelDashboard = New TableLayoutPanel()
+        SuspendLayout()
+        ' 
+        ' TableLayoutPanelDashboard
+        ' 
+        TableLayoutPanelDashboard.AutoScroll = True
+        TableLayoutPanelDashboard.ColumnCount = 4
+        TableLayoutPanelDashboard.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 25.0F))
+        TableLayoutPanelDashboard.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 25.0F))
+        TableLayoutPanelDashboard.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 25.0F))
+        TableLayoutPanelDashboard.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 25.0F))
+        TableLayoutPanelDashboard.Dock = DockStyle.Fill
+        TableLayoutPanelDashboard.Location = New Point(0, 0)
+        TableLayoutPanelDashboard.Name = "TableLayoutPanelDashboard"
+        TableLayoutPanelDashboard.RowCount = 5
+        TableLayoutPanelDashboard.RowStyles.Add(New RowStyle(SizeType.Percent, 20.0F))
+        TableLayoutPanelDashboard.RowStyles.Add(New RowStyle(SizeType.Percent, 20.0F))
+        TableLayoutPanelDashboard.RowStyles.Add(New RowStyle(SizeType.Percent, 20.0F))
+        TableLayoutPanelDashboard.RowStyles.Add(New RowStyle(SizeType.Percent, 20.0F))
+        TableLayoutPanelDashboard.RowStyles.Add(New RowStyle(SizeType.Percent, 20.0F))
+        TableLayoutPanelDashboard.Size = New Size(800, 450)
+        TableLayoutPanelDashboard.TabIndex = 0
+        ' 
+        ' FormMain
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(TableLayoutPanelDashboard)
+        Name = "FormMain"
+        Text = "Book Reader"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents TableLayoutPanelDashboard As TableLayoutPanel
+
+End Class
