75ffc40ead8b52e774baf0a2c08eb0e8801b4749
14thDream <dndelara@proton.me>
2024-12-15 22:20:03 +0800

    Added functionality to update books.

--- Book-Reader/FormSaveBook.vb
+++ Book-Reader/FormSaveBook.vb
@@ -7,6 +7,8 @@ Public Class FormSaveBook
     Private ConnectionString As String
     Private ImageLocation As String
 
+    Property BookId As Integer
+
     Public Sub New(connection As String, form As FormMain)
 
         ' This call is required by the designer.
@@ -17,6 +19,8 @@ Public Class FormSaveBook
 
         ConnectionString = connection
         ImageLocation = ""
+
+        BookId = -1
     End Sub
 
     Private Sub BookCover_PictureBox_Click(sender As Object, e As EventArgs) Handles BookCover_PictureBox.Click
@@ -45,7 +49,17 @@ Public Class FormSaveBook
         BookCover_PictureBox.Image = Nothing
     End Sub
 
-    Private Sub AddBook_Button_Click_1(sender As Object, e As EventArgs) Handles AddBook_Button.Click
+    Private Sub SaveBook_Button_Click(sender As Object, e As EventArgs) Handles AddBook_Button.Click
+        If BookId >= 0 Then
+            UpdateBook()
+        Else
+            AddBook()
+        End If
+
+        MainForm.LoadBooks()
+    End Sub
+
+    Private Sub AddBook()
         Dim SqlConnection = New MySqlConnection(ConnectionString)
 
         Dim Title = Title_TextBox.Text
@@ -82,8 +96,46 @@ Public Class FormSaveBook
         Catch ex As Exception
             MessageBox.Show(ex.Message)
         End Try
+    End Sub
 
-        MainForm.LoadBooks()
+    Private Sub UpdateBook()
+        Dim SqlConnection = New MySqlConnection(ConnectionString)
+
+        Dim Title = Title_TextBox.Text
+        Dim Author = If(Author_TextBox.Text <> "", Author_TextBox.Text, "Unnamed")
+        Dim Summary = Summary_TextBox.Text
+        Dim Genre = Genre_ComboBox.Text
+        Dim DatePublished = BookDatePublished.Value 'Check if it requires a default value
+
+        If Title = "" Then
+            MessageBox.Show("Title field required")
+            Return
+        ElseIf Genre_ComboBox.SelectedIndex = -1 Then
+            MessageBox.Show("Genre field required")
+            Return
+        End If
+
+        Try
+            SqlConnection.Open()
+
+            Dim Query = $"UPDATE Books SET Title = @Title, ImagePath = @ImagePath, Author = @Author, Genre = @Genre, DatePublished = @DatePublished, Summary = @Summary WHERE Id = @Id"
+            Dim Command = New MySqlCommand(Query, SqlConnection)
+
+            Command.Parameters.Add("@Id", BookId)
+            Command.Parameters.AddWithValue("@Title", Title)
+            Command.Parameters.AddWithValue("@ImagePath", ImageLocation)
+            Command.Parameters.AddWithValue("@Author", Author)
+            Command.Parameters.AddWithValue("@Genre", Genre)
+            Command.Parameters.AddWithValue("@DatePublished", DatePublished)
+            Command.Parameters.AddWithValue("@Summary", Summary)
+
+            Command.ExecuteReader()
+
+            SqlConnection.Close()
+            Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
     End Sub
 
     Private Sub AddBookForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
